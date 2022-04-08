41ddcdbbbb73c16f46010920992445a408abb0dc
kavindu <it20009540@my.sliit.lk>
2022-04-09 01:03:38 +0530

    slider and navbar

---
+++ frontend/src/Layouts/Layout1.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Navbar, Container, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap'
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const navbar = () => (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            {/* <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav> */}
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+        </Container>
+    </Navbar>
+)
+
+
+
+function Layout1(props) {
+    const onCollapse = () => {
+        setcollapsed(!collapsed)
+    }
+
+    const [collapsed, setcollapsed] = useState(true)
+
+
+    const slider = () => (
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            <div className="logo" />
+            <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                    Option 1
+                </Menu.Item>
+                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                    Option 2
+                </Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="9" icon={<FileOutlined />}>
+                    Files
+                </Menu.Item>
+            </Menu>
+        </Sider>
+    )
+
+    const breadcrumb = () => (
+        <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+        </Breadcrumb>
+    )
+
+    const footer = () => (
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    )
+
+    return (
+        <>
+            {navbar()}
+            <Layout style={{ minHeight: '100vh' }}>
+
+                {slider()}
+                <Layout className="site-layout">
+
+
+                    {breadcrumb()}
+
+                    {props.children}
+
+
+
+                </Layout>
+            </Layout>
+        </>
+
+    );
+}
+
+export default Layout1;
