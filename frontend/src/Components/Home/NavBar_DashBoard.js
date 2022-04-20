cb8ebd89baeeeca2903bb08cfdf42a68b4cbaae0
pramudnadula <pramud@surge.global>
2022-04-20 23:28:47 +0530

    changes 1

--- frontend/src/Components/Home/NavBar_DashBoard.js
+++ frontend/src/Components/Home/NavBar_DashBoard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Navbar, Container, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap'
-function NavBar(props) {
+import '../../Assets/Styles/NavBar_DashBoard.css'
+
+function NavBar_DashBoard(props) {
     return (
         <>
-            <Navbar className='headerNavBar'>
+             <Navbar  className='headerNavBar'>
                 <Container>
-                    <Navbar.Brand className='nav_icon' href="#home">RMS</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link className='home_nav' href="#home">Home</Nav.Link>
-                        <Nav.Link className='home_nav' href="#features">Features</Nav.Link>
-                        <Nav.Link className='home_nav' href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand href="#home">RMS</Navbar.Brand>
+                    {/* <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav> */}
                     <Form className="d-flex m-4">
                         <FormControl
                             type="search"
@@ -21,12 +23,11 @@ function NavBar(props) {
                         <Button variant="outline-success">Search</Button>
 
                     </Form>
-                    <button className='btn btn-light login_home pt-2 pb-2 p-4' >Login</button>
+                    {/* <button className='btn btn-light login_home pt-2 pb-2 p-4' >Login</button> */}
                 </Container>
             </Navbar>
-
         </>
     );
 }
 
-export default NavBar;
+export default NavBar_DashBoard;
---
+++ frontend/src/Components/Home/NavBar_Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap'
+import '../../Assets/Styles/NavBar_Home.css'
+
+function NavBar_Home(props) {
+    return (
+        <>
+            <Navbar className='headerNavBar_home'>
+                <Container>
+                    <Navbar.Brand className='nav_icon' href="#home">RMS</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link className='home_nav' href="#home">Home</Nav.Link>
+                        <Nav.Link className='home_nav' href="#features">Features</Nav.Link>
+                        <Nav.Link className='home_nav' href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex m-4">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+
+                    </Form>
+                    <button className='btn btn-light login_home pt-2 pb-2 p-4' >Login</button>
+                </Container>
+            </Navbar>
+
+        </>
+    );
+}
+
+export default NavBar_Home;
