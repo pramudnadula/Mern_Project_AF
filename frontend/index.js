cf0624cb2be2ca65c9a97794c78a645a8fd8fb2a
Pramud Nadula <86596641+pramudnadula@users.noreply.github.com>
2022-05-24 23:22:21 +0530

    Create index.js

---
+++ frontend/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+import App from './App';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
+import allreducers from './src/Reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+//STORE
+
+
+
+const store = createStore(
+  allreducers,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+createRoot(document.getElementById('app')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
