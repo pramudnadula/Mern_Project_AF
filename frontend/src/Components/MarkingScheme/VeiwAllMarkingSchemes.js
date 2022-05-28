b7a146af5b68a9dfb3396132f529a9f5c1260a35
RavinduMapatuna <IT20072674@my.sliit.lk>
2022-05-28 18:47:40 +0530

    rework

--- frontend/src/Components/MarkingScheme/VeiwAllMarkingSchemes.js
+++ frontend/src/Components/MarkingScheme/VeiwAllMarkingSchemes.js
@@ -16,35 +16,44 @@ function ViewAllMarkingSchemes(props) {
       });
   }, []);
 
+
   return (
+
     <div>
       <h1>All Marking Schemes</h1>
-      <table>
-        <tr>
-          <th>No</th>
-          <th>Name</th>
-          <th>Allocated Marks</th>
-          <th></th>
-        </tr>
-        {markingSchemes &&
-          markingSchemes.map((markingscheme, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{markingscheme.name}</td>
-              <td>{markingscheme.total}</td>
-              <td>
-                <Link to={`/markingscheme/view/${markingscheme._id}`}>
-                  <button>View</button>
-                </Link>
-              </td>
-            </tr>
-          ))}
+      <table class="table table-bordered table-secondary table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Submission Type</th>
+            <th>Allocated Marks</th>
+            <th>Creator</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {markingSchemes &&
+            markingSchemes.map((markingscheme, index) => (
+              <tr key={index}>
+                <td>{markingscheme.name}</td>
+                <td></td>
+                <td>{markingscheme.fullAllocatedMarks}</td>
+                <td>{markingscheme.creator}</td>
+                <td>
+                  <Link to={`/markingscheme/view/${markingscheme._id}`}>
+                    <button type="button" class="btn btn-outline-dark btn-sm">View</button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+
       </table>
       <br />
       <Link to={`/markingscheme/add`}>
-        <button>Add Marking Scheme</button>
+        <button type="button" class="btn btn-secondary btn-sm">Add Marking Scheme</button>
       </Link>
-    </div>
+    </div >
   );
 }
 
